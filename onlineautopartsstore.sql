9bb667af4c8a0f0022fc95dbab051a282eb9b4f8
vputtaparthi <vputtaparthi@gmail.com>
2016-12-06 09:50:24 -0600

    Added Export File To Source Files

--- onlineautopartsstore.sql
+++ onlineautopartsstore.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Nov 07, 2016 at 10:11 PM
+-- Generation Time: Dec 06, 2016 at 03:49 PM
 -- Server version: 5.5.49-log
 -- PHP Version: 7.0.9
 
@@ -30,8 +30,17 @@ USE `onlineautopartsstore`;
 
 CREATE TABLE IF NOT EXISTS `cart` (
   `user_ID` int(11) NOT NULL,
-  `item_ID` int(11) NOT NULL
+  `item_ID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`user_ID`, `item_ID`, `quantity`) VALUES
+(2, 9, 1),
+(6, 15, 1);
 
 -- --------------------------------------------------------
 
@@ -47,25 +56,24 @@ CREATE TABLE IF NOT EXISTS `item` (
   `category` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `tags` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`item_ID`, `item_name`, `price`, `num_available`, `category`, `description`, `tags`) VALUES
-(1, 'Grant 61035 Steering Wheel - Direct Fit', 258.39, 100, 'Interior Accessories', 'Series: Grant Revolution<br> Product Fit: Direct Fit<br> Grip Finish: Black and burlwood<br> Grip Material: Plastic <br> <br> GRANT REVOLUTION STEERING WHEEL, BLACK -- 4-spoke design; Includes installation instructions; Features a black polyurethane grip with perforated burlwood plastic inserts; Made to exacting standards to guarantee an OEM fit and finish; Utilizes your vehicle''''s existing airbag module, horn switch, and other switches; Can only be used on vehicles with factory switches; Factory switches not included.', 'interior steering wheel accessory');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `item_vehicle`
---
-
-CREATE TABLE IF NOT EXISTS `item_vehicle` (
-  `item_ID` int(11) NOT NULL,
-  `vehicle_ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(3, 'Duralast Break Pads', 24.99, 24, 'brake', 'This is a test brake', 'brake brakes duralast'),
+(4, 'Duralast Break Rotors', 39.99, 13, 'brake', 'This is a test rotor', 'brake brakes duralast rotor rotors'),
+(5, 'Duralast Break Hose', 13.5, 2, 'brake', 'This is a test brake hose', 'brake brakes duralast hose front'),
+(6, 'Surefire Engine', 499.99, 12, 'engine mechanical', 'This is a test engine', 'engine surefire sure fire '),
+(7, 'Valvoline Engine Oil', 6, 24, 'engine mechanical', 'This is a test engine oil', 'engine oil valvoline'),
+(8, 'Surefire Engine Cylinder Head', 500.99, 4, 'engine mechanical', 'This is a test engine head', 'engine sure fire surefire head cylinder'),
+(9, 'Walker Exhaust Pipe', 14.99, 3, 'exhaust', 'This is a test exhaust pipe', 'walker exhaust pipe'),
+(10, 'Thrush Turbo Exhaust Muffler', 13.99, 24, 'exhaust', 'This is a test exhaust muffler', 'exhaust'),
+(12, 'Wheel', 10.75, 9, 'steering', 'This is a test wheel', 'Tags'),
+(14, 'Autozone Transmission Fluid\r\n', 5.99, 22, 'transmission', 'This is a test transmission fluid ', 'transmission fluid automatic'),
+(15, 'Valvoline Transmission Fluid\r\n', 6.99, 17, 'transmission', 'transmission fluid test', 'transmission fluid test');
 
 -- --------------------------------------------------------
 
@@ -75,9 +83,34 @@ CREATE TABLE IF NOT EXISTS `item_vehicle` (
 
 CREATE TABLE IF NOT EXISTS `transaction` (
   `transaction_ID` int(11) NOT NULL,
-  `user_ID` int(11) NOT NULL,
-  `total_price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `date` varchar(100) NOT NULL,
+  `total_price` float NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `Zip` int(11) NOT NULL,
+  `credit_card` int(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`transaction_ID`, `date`, `total_price`, `address`, `city`, `state`, `Zip`, `credit_card`) VALUES
+(31, 'test date', 216500, '2800 Waterview Parkway', 'Richardson', 'Texas', 0, 0),
+(32, 'test date', 108250, '1234 royal lane', 'dallas', 'texas', 0, 0),
+(33, 'test date', 108261, '100 prince view', 'bedford', 'texas', 0, 0),
+(34, 'test date', 108250, 'ok', 'ok', 'ok', 0, 0),
+(35, 'test date', 108250, 'k', 'k', 'sa', 0, 0),
+(36, 'Monday 28th of November 2016 10:21:10 PM', 108250, 'date test', 'date test', 'date test', 0, 0),
+(37, 'Thursday 1st of December 2016 01:03:44 AM', 108261, 'ok test', 'ok test', 'ok test', 0, 0),
+(38, 'Thursday 1st of December 2016 02:02:42 AM', 108250, 'testing again', 'testting again', 'testing again', 0, 0),
+(39, 'Thursday 1st of December 2016 02:05:11 AM', 108250, 'one final time', 'one final time', 'one final time', 0, 0),
+(40, 'Thursday 1st of December 2016 02:05:12 AM', 108250, 'one final time', 'one final time', 'one final time', 0, 0),
+(41, 'Thursday 1st of December 2016 02:09:26 AM', 108250, 'Another test again', 'Another test again', 'Another test again', 0, 0),
+(42, 'Sunday 4th of December 2016 07:37:22 PM', 15.1442, 'ok', 'ok', 'ok', 0, 0),
+(43, 'Monday 5th of December 2016 07:10:55 PM', 7.56668, 'test', 'tes', 'test', 0, 0),
+(44, 'Monday 5th of December 2016 07:14:42 PM', 20.535, 'ok', 'ok', 'ok', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -91,6 +124,16 @@ CREATE TABLE IF NOT EXISTS `transaction_items` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `transaction_items`
+--
+
+INSERT INTO `transaction_items` (`transaction_ID`, `item_ID`, `quantity`) VALUES
+(42, 10, 1),
+(43, 15, 1),
+(44, 14, 1),
+(44, 15, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -102,14 +145,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `hashed_password` varchar(50) NOT NULL,
+  `hashed_password` varchar(200) NOT NULL,
   `phone_number` varchar(12) NOT NULL,
   `is_admin` tinyint(1) NOT NULL,
   `street_address` varchar(50) NOT NULL,
   `city` varchar(50) NOT NULL,
   `state` varchar(20) NOT NULL,
   `zip` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -117,8 +160,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`user_ID`, `first_name`, `last_name`, `email`, `hashed_password`, `phone_number`, `is_admin`, `street_address`, `city`, `state`, `zip`) VALUES
 (1, 'John', 'Smith', 'John.Smith@gmail.com', 'password', '1234567891', 0, '1234 fake', 'dallas', 'texas', 75021),
-(2, 'elon', 'musk', 'elon@tesla.com', 'password', '', 0, '', '', '', 0),
-(3, 'bill', 'gates', 'bill.gates@microsoft.com', '12345678', '', 0, '', '', '', 0);
+(2, 'elon', 'musk', 'elon@tesla.com', 'password', '', 1, '', '', '', 0),
+(3, 'bill', 'gates', 'bill.gates@microsoft.com', '12345678', '', 0, '', '', '', 0),
+(6, 'admin', 'admin', 'admin@gmail.com', '$2y$10$xHKgwjfRMmpsQ5fC3ZJIWedjvaGuXBQGzr6rTZwmHC4MfptYQk52a', '', 1, '', '', '', 0),
+(7, 'User', 'User', 'user@gmail.com', '$2y$10$fNr9IF65vj.rLAMB/3/pkuwp2Jq6BvRS7mg4G1ug1kZ8dHtshIfN2', '', 0, '', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -131,18 +176,21 @@ CREATE TABLE IF NOT EXISTS `user_transactions` (
   `transaction_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `vehicle`
+-- Dumping data for table `user_transactions`
 --
 
-CREATE TABLE IF NOT EXISTS `vehicle` (
-  `vehicle_ID` int(11) NOT NULL,
-  `make` varchar(50) NOT NULL,
-  `model` varchar(50) NOT NULL,
-  `year` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `user_transactions` (`user_ID`, `transaction_ID`) VALUES
+(2, 34),
+(2, 35),
+(2, 36),
+(2, 38),
+(2, 39),
+(2, 40),
+(2, 41),
+(2, 42),
+(7, 43),
+(6, 44);
 
 --
 -- Indexes for dumped tables
@@ -162,18 +210,11 @@ ALTER TABLE `item`
   ADD PRIMARY KEY (`item_ID`);
 
 --
--- Indexes for table `item_vehicle`
---
-ALTER TABLE `item_vehicle`
-  ADD PRIMARY KEY (`vehicle_ID`,`item_ID`) USING BTREE,
-  ADD KEY `Item_Vehicle Item Foreign Key` (`item_ID`);
-
---
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`transaction_ID`),
-  ADD KEY `user_ID` (`user_ID`);
+  ADD KEY `user_ID` (`date`);
 
 --
 -- Indexes for table `transaction_items`
@@ -197,12 +238,6 @@ ALTER TABLE `user_transactions`
   ADD KEY `User_Transactions Transaction Foreign Key` (`transaction_ID`);
 
 --
--- Indexes for table `vehicle`
---
-ALTER TABLE `vehicle`
-  ADD PRIMARY KEY (`vehicle_ID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -210,22 +245,17 @@ ALTER TABLE `vehicle`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `item_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transaction_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `vehicle`
---
-ALTER TABLE `vehicle`
-  MODIFY `vehicle_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- Constraints for dumped tables
 --
@@ -236,19 +266,6 @@ ALTER TABLE `vehicle`
 ALTER TABLE `cart`
   ADD CONSTRAINT `Cart Item Foreign Key` FOREIGN KEY (`item_ID`) REFERENCES `item` (`item_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Cart User Foreign Key` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `item_vehicle`
---
-ALTER TABLE `item_vehicle`
-  ADD CONSTRAINT `Item_Vehicle Item Foreign Key` FOREIGN KEY (`item_ID`) REFERENCES `item` (`item_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Item_Vehicle Vehicle Foreign Key` FOREIGN KEY (`vehicle_ID`) REFERENCES `vehicle` (`vehicle_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD CONSTRAINT `Transaction User Foreign Key` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaction_items`
